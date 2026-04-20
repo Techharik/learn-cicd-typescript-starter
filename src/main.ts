fd73185a133d4c43858381c39a227bea9b3f079f
Techharik <khariprasath30@gmail.com>
2026-04-20 12:32:54 +0530

    fix :Added the linting for the cli

--- src/main.ts
+++ src/main.ts
@@ -43,8 +43,7 @@ if (db) {
 v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
-function unused(){
-}
+function unused() {}
 
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
